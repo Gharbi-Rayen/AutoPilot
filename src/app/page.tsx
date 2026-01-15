77fbe113a38422b676bd7a9b26c445e9b6ae1dd6
Gharbi-Rayen <ggharbi323@gmail.com>
2026-01-15 10:09:25 +0100

    refactor: update dependencies and restructure TRPC setup
    - Downgraded @prisma/client and prisma versions for compatibility.
    - Added new dependencies: @tanstack/react-query, @trpc/client, @trpc/server, and others.
    - Removed old migration files and updated Prisma schema.
    - Implemented TRPC context and client setup for API handling.
    - Created Client component for user data fetching with React Query.
    - Enhanced layout with TRPC provider for context management.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
-import {Button} from "@/components/ui/button";
-
-const page = () =>{
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import {Client} from "./client";
+import {getQueryClient, trpc} from "@/trpc/server";
+const page = async() =>{
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+  
   return( 
   <div className="min-h-screen min-w-screen flex items-center justify-center ">
-    <Button>Click Me</Button>
-    </div>
-);
+  <HydrationBoundary state={dehydrate(queryClient)}>
+   <Client />
+  </HydrationBoundary>
+    </div>);
 };
 
 export default page ;
