09b36878b0b7791c450dfdd54440538a234b5465
Gharbi-Rayen <ggharbi323@gmail.com>
2026-01-22 12:03:42 +0100

    feat: integrate superjson for data transformation and enhance workflows UI with loading and error states

--- src/trpc/server.tsx
+++ src/trpc/server.tsx
@@ -5,6 +5,8 @@ import { createTRPCContext } from './init';
 import { makeQueryClient } from './query-client';
 import { appRouter } from './routers/_app';
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import superjson from 'superjson';
+
 // IMPORTANT: Create a stable getter for the query client that
 //            will return the same client during the same request.
 export const getQueryClient = cache(makeQueryClient);
