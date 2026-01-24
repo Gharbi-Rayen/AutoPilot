3abf1cae43dae513bff3dade5ecc263f893b22de
Gharbi-Rayen <ggharbi323@gmail.com>
2026-01-24 12:40:40 +0100

    feat: implement workflow editor with loading and error states, add prefetching and update workflow name functionality

--- src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
+++ src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
@@ -1,4 +1,10 @@
+import { Editor, EditorError, EditorLoading } from "@/features/editor/components/editor";
+import { prefetchWorkflow } from "@/features/workflows/server/prefetch";
+import { EditorHeader } from "@/features/editor/components/editor-header";
 import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface PageProps {
     
@@ -10,9 +16,26 @@ interface PageProps {
 const page = async ({params}: PageProps) => {
     await requireAuth();
  
-    const {workflowId       } = await params;
+    const { workflowId      } = await params;
 
-    return <p>workflow Id: {workflowId}</p>;
+    prefetchWorkflow(workflowId);
+
+    return (
+
+              <HydrateClient>
+                      <ErrorBoundary fallback={<EditorError />}>
+                          <Suspense fallback={<EditorLoading />}>
+                          <EditorHeader workflowId={workflowId} />
+                              <main className="flex-1">
+                                  <Editor workflowId={workflowId} />
+                              </main>
+                            </Suspense>
+                        </ErrorBoundary>
+
+            </HydrateClient>
+
+
+    )
    
 
 };
