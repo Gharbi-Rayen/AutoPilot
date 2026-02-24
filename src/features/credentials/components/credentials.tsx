1c39adffd58491df2baf85997c8d75882a7335cf
Gharbi-Rayen <ggharbi323@gmail.com>
2026-02-24 01:29:08 +0100

    feat: implement credential management components and functionality

---
+++ src/features/credentials/components/credentials.tsx
@@ -0,0 +1,152 @@
+"use client";
+
+import { formatDistanceToNow } from "date-fns";
+import { WorkflowIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import {
+  EmptyView,
+  EntityContainer,
+  EntityHeader,
+  EntityItem,
+  EntityList,
+  EntityPagination,
+  EntitySearch,
+  ErrorView,
+  LoadingView,
+} from "@/components/entity-components";
+import type { Workflow } from "@/generated/prisma";
+import { useEntitySearch } from "@/hooks/use-entity-search";
+import {
+ 
+  useRemoveCredential,
+  useSuspenseCredentials,
+} from "../hooks/use-credentials";
+import { useCredentialsParams } from "../hooks/use-credentials-params";
+
+export const CredentialsSearch = () => {
+  const [params, setParams] = useCredentialsParams();
+  const { searchvalue, onSearchChange } = useEntitySearch({
+    params,
+    setParams,
+  });
+  return (
+    <EntitySearch
+      placeholder="Search credentials..."
+      value={searchvalue}
+      onChange={onSearchChange}
+    />
+  );
+};
+
+export const CredentialsList = () => {
+  const credentials = useSuspenseCredentials();
+  return (
+    <EntityList
+      items={credentials.data.items}
+      getKey={(credential) => credential.id}
+      renderItem={(credential) => <CredentialItem data={credential} />}
+      emptyView={<CredentialsEmpty />}
+    />
+  );
+};
+
+export const CredentialsHeader = ({ disabled }: { disabled?: boolean }) => {
+
+  return (
+    
+      <EntityHeader
+        title="credentials"
+        description="create and manage your credentials"
+        newButtonLabel="New Credential"
+        disabled={disabled}
+        newButtonHref="/credentials/new"
+      />
+    
+  );
+};
+
+export const CredentialsPagination = () => {
+  const workflows = useSuspenseCredentials();
+  const [params, setParams] = useCredentialsParams();
+
+  return (
+    <EntityPagination
+      page={workflows.data.page}
+      totalPages={workflows.data.totalPages}
+      disabled={workflows.isFetching}
+      onPageChange={(page) => setParams({ ...params, page })}
+    />
+  );
+};
+
+export const CredentialsContainer = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <EntityContainer
+      header={<CredentialsHeader />}
+      search={<CredentialsSearch />}
+      pagination={<CredentialsPagination />}
+    >
+      {children}
+    </EntityContainer>
+  );
+};
+
+export const CredentialsLoading = () => {
+  return <LoadingView message="Loading credentials ..." />;
+};
+
+export const CredentialsError = () => {
+  return <ErrorView message="Error Loading credentials ..." />;
+};
+
+export const CredentialsEmpty = () => {
+  
+    const router = useRouter();
+    const handleCreate = () => {
+        router.push(`/credentials/new`);
+    };
+
+  return (
+    
+      <EmptyView
+        onNew={handleCreate}
+        message="you haven't created any credentials yet. get started by creating your first credential."
+      />
+    
+  );
+};
+
+
+
+export const CredentialItem = ({ data }: { data: Workflow }) => {
+  const removeCredential = useRemoveCredential();
+
+  const handleRemove = () => {
+    removeCredential.mutate({ id: data.id });
+  };
+
+  return (
+    <EntityItem
+      href={`/credentials/${data.id}`}
+      title={data.name}
+      subtitle={
+        <>
+          Updated {formatDistanceToNow(data.updatedAt, { addSuffix: true })}{" "}
+          &bull; created{" "}
+          {formatDistanceToNow(data.createdAt, { addSuffix: true })}
+        </>
+      }
+      image={
+        <div className="size-8 flex items-center justify-center">
+          <WorkflowIcon className="size-5 text-muted-foreground" />
+        </div>
+      }
+      onRemove={handleRemove}
+      isRemoving={removeCredential.isPending}
+    />
+  );
+};
